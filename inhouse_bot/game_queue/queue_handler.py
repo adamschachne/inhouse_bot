dff580606d093f2e2e0314025756d292afd9ac28
Adam Schachne <adamschachne@live.com>
2022-10-07 09:01:08 -0700

    change comment

--- inhouse_bot/game_queue/queue_handler.py
+++ inhouse_bot/game_queue/queue_handler.py
@@ -83,7 +83,9 @@ def add_player(
 
 def remove_player(player_id: int, channel_id: int = None, role: str = None):
     """
-    Removes the player from the queue in all roles in the channel
+    Removes the player from the queue in the channel
+
+    If a role is provided, the player is dropped from only that role
 
     If no channel id is given, drop him from *all* queues, cross-server
     """
