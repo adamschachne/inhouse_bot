6d909a869b74d67b481573c8a0b4e315631cf146
Adam Schachne <adamschachne@live.com>
2022-10-08 00:05:55 -0700

    enable autogenerate

--- alembic/env.py
+++ alembic/env.py
@@ -20,9 +20,9 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from inhouse_bot.database_orm import bot_declarative_base
+target_metadata = bot_declarative_base.metadata
+# target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
