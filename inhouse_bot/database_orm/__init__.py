f8313c309ce9460ac7861f273c04cb62e3cb4584
Adam Schachne <adamschachne@live.com>
2022-10-07 19:15:39 -0700

    remove old migration tool

--- inhouse_bot/database_orm/__init__.py
+++ inhouse_bot/database_orm/__init__.py
@@ -7,7 +7,3 @@ from inhouse_bot.database_orm.tables.player_rating import PlayerRating
 from inhouse_bot.database_orm.tables.server_config import ServerConfig
 from inhouse_bot.database_orm.tables.queue_player import QueuePlayer
 from inhouse_bot.database_orm.tables.channel_information import ChannelInformation
-
-from inhouse_bot.database_orm import mini_migration_tool
-
-mini_migration_tool.migrate()
