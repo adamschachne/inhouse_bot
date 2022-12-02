93b64b216186143e5de8d009c7ac1d5d2f6ecd0f
Adam Schachne <adamschachne@live.com>
2022-12-01 20:29:07 -0800

    Score game can take an optional Session

--- inhouse_bot/matchmaking_logic/score_game.py
+++ inhouse_bot/matchmaking_logic/score_game.py
@@ -1,11 +1,12 @@
 import trueskill
 
 from inhouse_bot.database_orm import session_scope
+from inhouse_bot.database_orm.session.session import Session
 from inhouse_bot.database_orm import Game
 from inhouse_bot.common_utils.get_last_game import get_last_game
 
 
-def update_trueskill(game: Game, session):
+def update_trueskill(game: Game, session: Session):
     """
     Updates the player’s rating based on the game’s result
     """
@@ -37,16 +38,19 @@ def update_trueskill(game: Game, session):
             session.merge(player_rating)
 
 
-def score_game_from_winning_player(player_id: int, server_id: int):
+def score_game_from_winning_player(player_id: int, server_id: int, session: Session | None = None):
     """
     Scores the last game of the player on the server as a *win*
     """
-    with session_scope() as session:
-        game, participant = get_last_game(player_id, server_id, session)
+    if session is None:
+        with session_scope() as session:
+            return score_game_from_winning_player(player_id, server_id, session)
 
-        if game and participant:
-            game.winner = participant.side
+    game, participant = get_last_game(player_id, server_id, session)
 
-            update_trueskill(game, session)
+    if game and participant:
+        game.winner = participant.side
+
+        update_trueskill(game, session)
 
         # Commit will happen here
